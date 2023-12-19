96c27ae1f75030fe2d73936960dda05bdf8e56ac
Marina Jakčin <marina.jakcin@applifting.cz>
2023-12-19 15:47:05 +0100

    baton verkada v0

--- pkg/connector/resource_types.go
+++ pkg/connector/resource_types.go
@@ -4,9 +4,16 @@ import (
 	v2 "github.com/conductorone/baton-sdk/pb/c1/connector/v2"
 )
 
-// The user resource type is for all user objects from the database.
-var userResourceType = &v2.ResourceType{
-	Id:          "user",
-	DisplayName: "User",
-	Traits:      []v2.ResourceType_Trait{v2.ResourceType_TRAIT_USER},
-}
+var (
+	userResourceType = &v2.ResourceType{
+		Id:          "user",
+		DisplayName: "User",
+		Traits:      []v2.ResourceType_Trait{v2.ResourceType_TRAIT_USER},
+		Annotations: annotationsForUserResourceType(),
+	}
+	groupResourceType = &v2.ResourceType{
+		Id:          "group",
+		DisplayName: "Group",
+		Traits:      []v2.ResourceType_Trait{v2.ResourceType_TRAIT_GROUP},
+	}
+)
